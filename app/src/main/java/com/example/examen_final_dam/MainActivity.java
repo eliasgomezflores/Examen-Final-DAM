9585e161de637ede3651676a52eb293263a3af31
Orlando <orlando97rm@gmail.com>
2021-02-11 14:39:04 -0500

    Jose Orlando Ramos Machuca

--- app/src/main/java/com/example/examen_final_dam/MainActivity.java
+++ app/src/main/java/com/example/examen_final_dam/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.examen_final_dam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.button);
+
+        // Definir el evento click sobre el botón de login
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Formulario.class);
+                startActivity(i);
+            }
+        });
     }
 }
